f2d120d415ce8f3ac72709c7361f6032e9573740
Jakub Gereg <j.gereg@gmail.com>
2024-03-03 10:51:55 +0100

    rename customComponents to mapping

--- src/template-builder.ts
+++ src/template-builder.ts
@@ -4,11 +4,11 @@ import { ComponentTemplate, CustomComponentMapping } from './types';
 import { defineComponent, isHtmlElement } from './utils';
 
 const renderComponent =
-  (customComponents?: CustomComponentMapping) =>
+  (mapping?: CustomComponentMapping) =>
   ({ type = 'div', className, children: childrenFromProps, props }: ComponentTemplate): React.ReactElement => {
     const children = Array.isArray(childrenFromProps)
       ? childrenFromProps.map((child, idx) =>
-          React.createElement(React.Fragment, { key: idx }, renderComponent(customComponents)(child))
+          React.createElement(React.Fragment, { key: idx }, renderComponent(mapping)(child))
         )
       : childrenFromProps;
 
@@ -16,15 +16,15 @@ const renderComponent =
 
     const combinedProps = { ...props, className };
     if (isHtmlElement(type)) return React.createElement(type, combinedProps, children);
-    if (customComponents && customComponents[type]) {
-      const Component = customComponents[type];
+    if (mapping && mapping[type]) {
+      const Component = mapping[type];
       return React.createElement(Component, combinedProps, children);
     }
     console.error(`Component type=[${type}] not found`);
     return React.createElement('div', combinedProps, children);
   };
 
-export const createTemplateBuilder = (customComponents?: CustomComponentMapping) => ({
-  renderComponent: renderComponent(customComponents),
-  defineComponent: defineComponent
+export const createTemplateBuilder = (mapping?: CustomComponentMapping) => ({
+  renderComponent: renderComponent(mapping),
+  defineComponent
 });
